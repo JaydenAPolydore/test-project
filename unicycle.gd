73f9bf9c75a526b47d01393e29146bcf4b41e8d4
Jayden Polydore <polydore.jayden@gmail.com>
2025-06-05 11:57:23 -0400

    im jusst testing

--- unicycle.gd
+++
@@ -1,8 +0,0 @@
-extends VehicleBody3D
-
-@export var MAX_STEER = 0.9
-@export var ENGINE_POWER = 300
-
-func _physics_process(delta):
-	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 10)
-	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
